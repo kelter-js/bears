37570cbe375b615e3a48452da273a37efaaff807
kelter-js <spawnkillerqw@gmail.com>
2024-08-01 22:07:08 +0500

    feat: start to implement header component

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -1,1 +1,27 @@
-export const Template = () => {};
+import styled from "styled-components";
+import { device } from "../../helpers";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  box-shadow: 0 1px 0 0 var(--grey-shadow), inset 0 1px 0 0 var(--grey-shadow);
+
+  @media ${device.mobile} and ${device.maxMobile} {
+    height: 79px;
+
+    & .svg-icon {
+      height: 35px;
+      width: 86px;
+    }
+  }
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 32px;
+  right: 31px;
+  width: 20px;
+  height: 14px;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,16 @@
-export const Header = () => {};
+import { useState } from "react";
+import { HeaderLogo } from "../HeaderLogo";
+
+import * as S from "./Header.styled";
+
+export const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <S.HeaderContainer>
+      <HeaderLogo />
+
+      <S.Menu>123</S.Menu>
+    </S.HeaderContainer>
+  );
+};
