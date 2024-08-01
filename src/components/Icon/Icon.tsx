ad446b5c49c403f33b9134f5f6acaf9705dc048c
kelter-js <spawnkillerqw@gmail.com>
2024-08-01 22:07:51 +0500

    feat: implemented future icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,1 +1,10 @@
-export const Icon = () => {};
+import { FC } from "react";
+
+interface IconProps {
+  iconPath?: string;
+  iconName?: string;
+}
+
+export const Icon: FC<IconProps> = ({ iconPath, iconName }) => (
+  <img src={iconPath ?? iconName} className="svg-icon" />
+);
