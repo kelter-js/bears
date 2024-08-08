6ccd944cf2abd33ce708e618670924df5f770b7f
kelter-js <spawnkillerqw@gmail.com>
2024-08-08 21:04:34 +0500

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { Header } from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Main } from "./modules/Main";
+import { Form } from "./modules/Form";
+import { Catalog } from "./modules/Catalog";
+import { PAGES } from "./constants/enums";
+
 import "./index.css";
 
-const App = () => {
-  return <Header />;
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={PAGES.HOME} element={<Main />} />
+      <Route path={PAGES.CATALOG} element={<Form />} />
+      <Route path={PAGES.FORM} element={<Catalog />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
