631d90d2708d73eee5319790a6866627ae1da5ad
kelter-js <spawnkillerqw@gmail.com>
2024-08-08 21:04:51 +0500

    feat: added first implementation of header menu

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 import { device } from "../../helpers";
 
 export const HeaderContainer = styled.div`
@@ -18,10 +19,44 @@ export const HeaderContainer = styled.div`
   }
 `;
 
-export const Menu = styled.div`
+export const Menu = styled("div").withConfig({
+  shouldForwardProp: (prop) => prop !== "isOpen",
+})<{ isOpen: boolean }>`
   position: absolute;
   top: 32px;
   right: 31px;
   width: 20px;
   height: 14px;
+  cursor: pointer;
+
+  &::before,
+  &::after {
+    content: "";
+    left: 0;
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 2px;
+    background: #000;
+  }
+
+  &::before {
+    top: ${(props) => (props.isOpen ? "6px" : 0)};
+    transform: ${(props) => (props.isOpen ? "rotate(45deg)" : "unset")};
+    box-shadow: ${(props) =>
+      props.isOpen ? "0 6px 0 rgba(0,0,0,0)" : "0 6px 0 #000"};
+    transition: ${(props) =>
+      props.isOpen
+        ? "box-shadow .15s, top .1s, transform .1s .15s"
+        : "box-shadow .1s .15s, top .1s .15s, transform .1s"};
+  }
+
+  &::after {
+    bottom: ${(props) => (props.isOpen ? "6px" : 0)};
+    transition: ${(props) =>
+      props.isOpen
+        ? "bottom .1s, transform .1s .15s"
+        : "bottom .1s .15s, transform .1s"};
+    transform: ${(props) => (props.isOpen ? "rotate(-45deg)" : "unset")};
+  }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,42 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { HeaderLogo } from "../HeaderLogo";
 
+import { IconButton } from "../IconButton";
+import { PAGES } from "../../constants/enums";
 import * as S from "./Header.styled";
 
 export const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const toggleMenu = () => setMenuOpen((state) => !state);
 
   return (
-    <S.HeaderContainer>
-      <HeaderLogo />
+    <div>
+      <S.HeaderContainer>
+        <HeaderLogo />
 
-      <S.Menu>123</S.Menu>
-    </S.HeaderContainer>
+        <S.Menu isOpen={isMenuOpen}>
+          <IconButton onClick={toggleMenu} />
+        </S.Menu>
+      </S.HeaderContainer>
+
+      {isMenuOpen && (
+        <nav>
+          <ul>
+            <li>
+              <IconButton onClick={() => navigate(PAGES.CATALOG)}>
+                Каталог товаров
+              </IconButton>
+            </li>
+            <li>
+              <IconButton onClick={() => navigate(PAGES.FORM)}>
+                Вязание на заказ
+              </IconButton>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 };
