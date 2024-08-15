64d1465923b47e07b6cf09f42b7357750c00d21a
kelter-js <spawnkillerqw@gmail.com>
2024-08-15 21:07:03 +0500

    feat: split menu logic and layout into standalone component

--- src/components/Menu/Menu.styled.tsx
+++ src/components/Menu/Menu.styled.tsx
@@ -1,1 +1,89 @@
-export const Template = () => {};
+import styled from "styled-components";
+
+export const Menu = styled("div").withConfig({
+  shouldForwardProp: (prop) => prop !== "isOpen",
+})<{ isOpen: boolean }>`
+  position: absolute;
+  top: 32px;
+  right: 31px;
+  width: 20px;
+  height: 14px;
+  cursor: pointer;
+
+  & .toggle-button {
+    position: relative;
+    top: -13px;
+    left: -10px;
+    height: 40px;
+    width: 40px;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    left: 0;
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 2px;
+    background: #000;
+  }
+
+  &::before {
+    top: ${(props) => (props.isOpen ? "8px" : 0)};
+    transform: ${(props) => (props.isOpen ? "rotate(45deg)" : "unset")};
+    box-shadow: ${(props) =>
+      props.isOpen ? "0 6px 0 rgba(0,0,0,0)" : "0 6px 0 var(--black)"};
+    transition: ${(props) =>
+      props.isOpen
+        ? "box-shadow .15s, top .1s, transform .1s .15s"
+        : "box-shadow .1s .15s, top .1s .15s, transform .1s"};
+  }
+
+  &::after {
+    bottom: ${(props) => (props.isOpen ? "4px" : 0)};
+    transition: ${(props) =>
+      props.isOpen
+        ? "bottom .1s, transform .1s .15s"
+        : "bottom .1s .15s, transform .1s"};
+    transform: ${(props) => (props.isOpen ? "rotate(-45deg)" : "unset")};
+  }
+`;
+
+export const NavContainer = styled("nav")`
+  position: absolute;
+  top: 79px;
+  width: 100%;
+`;
+
+export const MenuItem = styled("li")`
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 79px;
+  width: 100%;
+  box-shadow: 0 1px 0 0 var(--grey-shadow), inset 0 1px 0 0 var(--grey-shadow);
+
+  & .catalog-item {
+    margin-left: 76px;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 24px;
+    color: var(--black);
+  }
+
+  & .search-icon,
+  & .cart-icon {
+    position: absolute;
+    left: 31px;
+    height: 18px;
+  }
+
+  & .search-icon {
+    width: 18px;
+  }
+
+  & .cart-icon {
+    width: 21px;
+  }
+`;
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,1 +1,57 @@
-export const Menu = () => {};
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { PAGES } from "../../constants/enums";
+import { IconButton } from "../IconButton";
+import { Icon } from "../Icon";
+import AdaptiveWrapper from "../common/AdaptiveWrapper";
+import Typography from "../common/Typography";
+import * as S from "./Menu.styled";
+
+export const Menu = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((state) => !state);
+
+  return (
+    <AdaptiveWrapper renderResolutions={{ mobile: true }}>
+      <S.Menu isOpen={isMenuOpen}>
+        <IconButton className="toggle-button" onClick={toggleMenu} />
+      </S.Menu>
+
+      {isMenuOpen && (
+        <S.NavContainer>
+          <ul>
+            <S.MenuItem>
+              <Link to={PAGES.CATALOG}>
+                <Typography className="catalog-item">
+                  Каталог товаров
+                </Typography>
+              </Link>
+            </S.MenuItem>
+
+            <S.MenuItem>
+              <Link to={PAGES.FORM}>
+                <Typography className="catalog-item">
+                  Вязание на заказ
+                </Typography>
+              </Link>
+            </S.MenuItem>
+
+            <S.MenuItem>
+              <Icon className="search-icon" iconPath={`/icons/search.svg`} />
+              <Typography className="catalog-item">Поиск по сайту</Typography>
+            </S.MenuItem>
+
+            <S.MenuItem>
+              <Icon className="cart-icon" iconPath={`/icons/cart.svg`} />
+              <Typography className="catalog-item">
+                Корзина: пока пуста
+              </Typography>
+            </S.MenuItem>
+          </ul>
+        </S.NavContainer>
+      )}
+    </AdaptiveWrapper>
+  );
+};
