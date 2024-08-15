8d9b2532820089297eb5b0879af61228fc4e385e
kelter-js <spawnkillerqw@gmail.com>
2024-08-15 21:07:24 +0500

    feat: moved logic outside of header for menu component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,14 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { HeaderLogo } from "../HeaderLogo";
+import { Menu } from "../Menu";
 
-import { IconButton } from "../IconButton";
-import { PAGES } from "../../constants/enums";
 import * as S from "./Header.styled";
 
-export const Header = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const navigate = useNavigate();
-  const toggleMenu = () => setMenuOpen((state) => !state);
+export const Header = () => (
+  <div>
+    <S.HeaderContainer>
+      <HeaderLogo />
 
-  return (
-    <div>
-      <S.HeaderContainer>
-        <HeaderLogo />
-
-        <S.Menu isOpen={isMenuOpen}>
-          <IconButton onClick={toggleMenu} />
-        </S.Menu>
-      </S.HeaderContainer>
-
-      {isMenuOpen && (
-        <nav>
-          <ul>
-            <li>
-              <IconButton onClick={() => navigate(PAGES.CATALOG)}>
-                Каталог товаров
-              </IconButton>
-            </li>
-            <li>
-              <IconButton onClick={() => navigate(PAGES.FORM)}>
-                Вязание на заказ
-              </IconButton>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </div>
-  );
-};
+      <Menu />
+    </S.HeaderContainer>
+  </div>
+);
