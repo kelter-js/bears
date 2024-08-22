020ca51f7b19176c068850bd2454e4589b9db446
kelter-js <spawnkillerqw@gmail.com>
2024-08-22 19:39:37 +0500

    feat: header now use context value

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
+import { useApp } from "../../Context/AppContext";
 import { HeaderLogo } from "../HeaderLogo";
 import { Menu } from "../Menu";
 
 import * as S from "./Header.styled";
 
-export const Header = () => (
-  <div>
-    <S.HeaderContainer>
-      <HeaderLogo />
+export const Header = () => {
+  const { isMenuOpen } = useApp();
 
-      <Menu />
-    </S.HeaderContainer>
-  </div>
-);
+  return (
+    <div>
+      <S.HeaderContainer isOpen={isMenuOpen}>
+        <HeaderLogo />
+
+        <Menu />
+      </S.HeaderContainer>
+    </div>
+  );
+};
