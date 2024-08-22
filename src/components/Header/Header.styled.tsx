2044e80c195c5e2dba8b5544d9f5e1312604226f
kelter-js <spawnkillerqw@gmail.com>
2024-08-22 19:39:23 +0500

    feat: header now receive dynamic height

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -2,17 +2,20 @@ import styled from "styled-components";
 
 import { device } from "../../helpers";
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled("div").withConfig({
+  shouldForwardProp: (prop) => prop !== "isOpen",
+})<{ isOpen: boolean }>`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 
   box-shadow: 0 1px 0 0 var(--grey-shadow), inset 0 1px 0 0 var(--grey-shadow);
 
   @media ${device.mobile} and ${device.maxMobile} {
-    height: 79px;
+    height: ${(props) => (props.isOpen ? "395px" : "79px")};
 
     & .svg-icon {
+      margin-top: 22px;
       height: 35px;
       width: 86px;
     }
