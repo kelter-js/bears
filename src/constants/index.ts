611d9d13e630972708a282e73576b85d53109020
kelter-js <spawnkillerqw@gmail.com>
2024-08-29 20:39:00 +0500

    feat: added constants to render purposes

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+import { PromoData } from "../types";
+
+export const PROMO_ITEMS: PromoData[] = [
+  {
+    description: "Предметы  интерьера",
+    pathIcon: "/icons/interior.svg",
+    className: "promo-interier",
+    id: 0,
+  },
+  {
+    description: "Детские  игрушки",
+    pathIcon: "/icons/toy.svg",
+    className: "promo-toy",
+    id: 1,
+  },
+];
