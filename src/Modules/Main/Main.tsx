6c5fa50d7fdb9248164bf04472780d2b2ab9b494
kelter-js <spawnkillerqw@gmail.com>
2024-09-12 21:05:53 +0500

    feat: added new component render on main page

--- src/Modules/Main/Main.tsx
+++ src/Modules/Main/Main.tsx
@@ -1,11 +1,13 @@
+import { MainItemCard } from "../../components/MainItemCard";
 import { Header } from "../../components/Header";
 import { Promo } from "../../components/Promo";
+import { Features } from "../../components/Features";
 
-export const Main = () => {
-  return (
-    <div>
-      <Header />
-      <Promo />
-    </div>
-  );
-};
+export const Main = () => (
+  <div>
+    <Header />
+    <Promo />
+    <MainItemCard />
+    <Features />
+  </div>
+);
