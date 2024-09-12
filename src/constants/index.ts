78a35457fef113794b6aff35b1a4478f8a94e00f
kelter-js <spawnkillerqw@gmail.com>
2024-09-12 21:05:41 +0500

    feat: added new constants for rendering purposes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { PromoData } from "../types";
+import { FeatureData, PromoData } from "../types";
 
 export const PROMO_ITEMS: PromoData[] = [
   {
@@ -12,5 +12,44 @@ export const PROMO_ITEMS: PromoData[] = [
     pathIcon: "/icons/toy.svg",
     className: "promo-toy",
     id: 1,
+  },
+];
+
+export const FEATURE_ITEMS: FeatureData[] = [
+  {
+    title: "Экологически  чистые материалы",
+    pathIcon: "/icons/flower.svg",
+    className: "feature-flower",
+    id: 0,
+  },
+  {
+    title: "Скандинавский стиль  по российской цене",
+    pathIcon: "/icons/money.svg",
+    className: "feature-money",
+    id: 1,
+  },
+  {
+    title: "Увеличивает лайки на фотографиях",
+    pathIcon: "/icons/heart.svg",
+    className: "feature-photo",
+    id: 2,
+  },
+  {
+    title: "Связано вручную с  любовью и умилением",
+    pathIcon: "/icons/chain.svg",
+    className: "feature-ball",
+    id: 3,
+  },
+  {
+    title: "Поддержка\u00A0отечественного производителя",
+    pathIcon: "/icons/rocket.svg",
+    className: "feature-rocket",
+    id: 4,
+  },
+  {
+    title: "Поставляется в  подарочной упаковке",
+    pathIcon: "/icons/gift.svg",
+    className: "feature-gift",
+    id: 5,
   },
 ];
