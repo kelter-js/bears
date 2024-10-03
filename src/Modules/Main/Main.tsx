3d2ced047ea2a5bbdcdf64863582f4d8a386cf8b
kelter-js <spawnkillerqw@gmail.com>
2024-10-03 20:34:55 +0500

    fix: merge conflicts

--- src/Modules/Main/Main.tsx
+++ src/Modules/Main/Main.tsx
@@ -4,10 +4,7 @@ import { Promo } from "../../components/Promo";
 import { Features } from "../../components/Features";
 import { Feedback } from "../../components/Feedback";
 import { Contacts } from "../../components/Contacts";
-<<<<<<< HEAD
 import { Location } from "../../components/Location";
-=======
->>>>>>> 03ac7587286ffe5aabcf02c9102b875994decf8e
 
 export const Main = () => (
   <div>
@@ -17,9 +14,6 @@ export const Main = () => (
     <Features />
     <Feedback />
     <Contacts />
-<<<<<<< HEAD
     <Location />
-=======
->>>>>>> 03ac7587286ffe5aabcf02c9102b875994decf8e
   </div>
 );
