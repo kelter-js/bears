7776a5f9a060c7e076de43d438c83756d905f6dd
kelter-js <spawnkillerqw@gmail.com>
2024-10-24 15:05:35 +0500

    feat: added catalog render list

--- src/Modules/Catalog/Catalog.tsx
+++ src/Modules/Catalog/Catalog.tsx
@@ -1,11 +1,13 @@
 import { CatalogTeaser } from "../../components/CatalogTeaser";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { Productions } from "../../components/Productions";
 
 export const Catalog = () => (
   <div>
     <Header />
     <CatalogTeaser />
+    <Productions />
     <Footer />
   </div>
 );
