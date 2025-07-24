62de3dd4c6482650d57e86116cd3c0af53ea035c
kelter-js <spawnkillerqw@gmail.com>
2025-07-24 17:08:25 +0500

    feat: added scrollbar width hook usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { useScrollbarWidth } from "./hooks/useScrollbarWidth";
 import { AppContextProvider } from "./Context/AppContext";
 import { Catalog } from "./modules/Catalog";
 import { PAGES } from "./constants/enums";
 import { Main } from "./modules/Main";
 import { Form } from "./modules/Form";
-
 import "./index.css";
 
-const App = () => (
-  <AppContextProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path={PAGES.HOME} element={<Main />} />
-        <Route path={PAGES.FORM} element={<Form />} />
-        <Route path={PAGES.CATALOG} element={<Catalog />} />
-      </Routes>
-    </BrowserRouter>
-  </AppContextProvider>
-);
+const App = () => {
+  useScrollbarWidth();
+
+  return (
+    <AppContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={PAGES.HOME} element={<Main />} />
+          <Route path={PAGES.FORM} element={<Form />} />
+          <Route path={PAGES.CATALOG} element={<Catalog />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContextProvider>
+  );
+};
 
 export default App;
