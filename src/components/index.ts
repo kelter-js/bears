2392d5f18f439f1b538ea4930aa6a0b6917efec8
kelter-js <spawnkillerqw@gmail.com>
2025-07-24 17:09:50 +0500

    feat: components reexports

---
+++ src/components/index.ts
@@ -0,0 +1,27 @@
+import { AboutProcess } from "./AboutProcess";
+import { MainItemCard } from "./MainItemCard";
+import { Productions } from "./Productions";
+import { FormOrder } from "./FormOrder";
+import { Feedback } from "./Feedback";
+import { Features } from "./Features";
+import { Contacts } from "./Contacts";
+import { Location } from "./Location";
+import { Teaser } from "./Teaser";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Promo } from "./Promo";
+
+export {
+  AboutProcess,
+  MainItemCard,
+  Productions,
+  FormOrder,
+  Header,
+  Promo,
+  Features,
+  Feedback,
+  Contacts,
+  Location,
+  Footer,
+  Teaser,
+};
