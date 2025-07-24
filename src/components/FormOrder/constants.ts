bee0cae57ac35c2b264e4f74f706a24e0e1a91e3
kelter-js <spawnkillerqw@gmail.com>
2025-07-24 17:14:40 +0500

    feat: added few more constants and styled-components export

---
+++ src/components/FormOrder/constants.ts
@@ -0,0 +1,25 @@
+export const DEFAULT_VALUES = {
+  type: "accessories",
+  colors: {
+    white: true,
+    gray: true,
+    tiffany: false,
+    black: false,
+    pink: false,
+    orange: false,
+  },
+  firstName: "",
+  lastName: "",
+  surName: "",
+  phoneNumber: "",
+  email: "",
+};
+
+export const COLORS = [
+  { name: "white", label: "Белый" },
+  { name: "gray", label: "Серый" },
+  { name: "tiffany", label: "«Тиффани»" },
+  { name: "black", label: "Черный" },
+  { name: "pink", label: "Розовый" },
+  { name: "orange", label: "Оранжевый" },
+];
--- src/components/Menu/Menu.styled.tsx
+++ src/components/Menu/Menu.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../../helpers";
 
 export const Menu = styled("div").withConfig({
   shouldForwardProp: (prop) => prop !== "isOpen",
@@ -57,7 +58,40 @@ export const NavContainer = styled("nav")`
   width: 100%;
 `;
 
-export const MenuItem = styled("li")`
+export const AdditionalOptionsContainer = styled("div")`
+  display: flex;
+  gap: 45px;
+
+  @media ${device.desktop} {
+    position: absolute;
+    left: 51px;
+    top: 107px;
+    width: 349px;
+    z-index: 1;
+
+    & .additional-option {
+      font-family: var(--font-family);
+      font-weight: 700;
+      font-size: 15px;
+
+      line-height: 24px;
+      color: var(--black);
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.6;
+      }
+
+      &:active {
+        opacity: 0.3;
+      }
+    }
+  }
+`;
+
+export const MenuItem = styled("li").withConfig({
+  shouldForwardProp: (prop) => prop !== "isButton",
+})<{ isButton?: boolean }>`
   position: relative;
   display: flex;
   align-items: center;
@@ -65,12 +99,41 @@ export const MenuItem = styled("li")`
   width: 100%;
   box-shadow: 0 1px 0 0 var(--grey-shadow), inset 0 1px 0 0 var(--grey-shadow);
 
-  & .catalog-item {
-    margin-left: 76px;
-    font-weight: 700;
-    font-size: 17px;
-    line-height: 24px;
-    color: var(--black);
+  @media ${device.tablet} and ${device.maxTablet} {
+    height: ${(props) => (props.isButton ? "88px" : "auto")};
+    min-width: ${(props) => (props.isButton ? "82px" : "auto")};
+    cursor: ${(props) => (props.isButton ? "pointer" : "auto")};
+
+    border-left: 1px solid var(--fade-gray);
+    box-shadow: none;
+  }
+
+  @media ${device.desktop} {
+    height: ${(props) => (props.isButton ? "88px" : "auto")};
+    min-width: ${(props) => (props.isButton ? "82px" : "auto")};
+    cursor: ${(props) => (props.isButton ? "pointer" : "auto")};
+
+    border-left: 1px solid var(--fade-gray);
+    box-shadow: none;
+  }
+
+  &.cart-item {
+    &:hover .catalog-item {
+      opacity: 0.6;
+    }
+
+    &:hover .cart-icon {
+      color: #63d1bb;
+    }
+
+    &:active .cart-icon {
+      color: #63d1bb;
+      opacity: 0.3;
+    }
+
+    &:active .catalog-item {
+      opacity: 0.3;
+    }
   }
 
   & .search-icon,
@@ -78,6 +141,38 @@ export const MenuItem = styled("li")`
     position: absolute;
     left: 31px;
     height: 18px;
+    color: black;
+  }
+
+  &:hover .search-icon {
+    color: #63d1bb;
+  }
+
+  &:active .search-icon {
+    color: #63d1bb;
+    opacity: 0.3;
+  }
+
+  & .catalog-item {
+    margin-left: 76px;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 24px;
+    color: var(--black);
+
+    @media ${device.tablet} and ${device.maxTablet} {
+      min-width: 182px;
+      margin-left: 82px;
+      white-space: nowrap;
+      text-align: end;
+    }
+
+    @media ${device.desktop} {
+      min-width: 182px;
+      margin-left: 82px;
+      white-space: nowrap;
+      text-align: end;
+    }
   }
 
   & .search-icon {
@@ -86,5 +181,67 @@ export const MenuItem = styled("li")`
 
   & .cart-icon {
     width: 21px;
+  }
+`;
+
+export const Actions = styled("div")`
+  display: flex;
+  margin-left: auto;
+`;
+
+export const LinksList = styled("div")`
+  position: absolute;
+  bottom: -44.5px;
+  left: 50px;
+  display: flex;
+  z-index: 1;
+
+  & .catalog-link {
+    display: block;
+    min-width: 167px;
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 15px;
+
+    line-height: 24px;
+    color: var(--black);
+
+    &:hover {
+      opacity: 0.6;
+    }
+
+    &:active {
+      opacity: 0.3;
+    }
+  }
+
+  @media ${device.desktop} {
+    bottom: unset;
+    width: 358px;
+    justify-content: space-between;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: -32px;
+      left: 50%;
+      display: block;
+      height: 88px;
+      width: 1px;
+      background-color: var(--black);
+      opacity: 0.1;
+    }
+
+    & .catalog-link {
+      position: relative;
+      display: block;
+      min-width: 149px;
+      font-family: var(--font-family);
+      font-weight: 700;
+      font-size: 17px;
+
+      line-height: 24px;
+      color: var(--black);
+    }
   }
 `;
